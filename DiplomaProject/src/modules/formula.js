22b542ec51365407921ab54e9c23e4faeaa3ca81
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-16 21:12:20 +0300

    Insert animation for fotmula cards.

--- DiplomaProject/src/modules/formula.js
+++ DiplomaProject/src/modules/formula.js
@@ -4,28 +4,42 @@ const formulaBlockActions = () => {
         formulaItemCards = dtFormulaField.querySelectorAll('.formula-item-popup');
         
     formulaItems.forEach((item) => {
-        item.addEventListener('mouseenter', (event) => {
+        item.addEventListener('click', (event) => {
             let target = event.target;
             
             formulaItemCards.forEach(card => {
                 if(card.className.substr(-2) === target.textContent){
-                    card.style.visibility = 'visible';
-                    card.style.opacity = '1';
+                    if(Math.floor(card.getBoundingClientRect().top) > 0){
+                        card.style.visibility = 'visible';
+                        card.style.opacity = '1';
+                    }else{
+                        console.dir(card.children[1]);
+                        for(let i = 0; i < card.children.length; i++){
+                            card.children[i].className = 'rotate-formula-card-par';
+                        }
+
+                        card.classList.add('rotate-formula-card');;
+                        card.style.visibility = 'visible';
+                        card.style.opacity = '1';
+                    }
+                    
                 }
             });
 
         });
         
-        item.addEventListener('mouseleave', (event) => {
-            let target = event.target;
+        // item.addEventListener('mouseleave', (event) => {
+        //     let target = event.target;
     
-            formulaItemCards.forEach(card => {
-                if(card.className.substr(-2) === target.textContent){
-                    card.style.visibility = 'hidden';
-                    card.style.opacity = '0';
-                }
-            });
-        });
+        //     formulaItemCards.forEach(card => {
+        //         if(card.className.search(`${target.textContent}`) >= 0){
+        //             console.log('hi');
+        //             card.style.visibility = 'hidden';
+        //             card.style.opacity = '0';
+        //             card.classList.remove('rotate-formula-card');
+        //         }
+        //     });
+        // });
     })
 };
 
