94a01a1206847a245ed0bda98a1cbddf02f077a6
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-17 16:24:35 +0300

    Restyled accordion function.

--- DiplomaProject/src/modules/accordion.js
+++ DiplomaProject/src/modules/accordion.js
@@ -1,21 +1,20 @@
 const accordionSwitch = () => {
     const accordion = document.querySelector('.accordion'),
-        allHeaders = accordion.querySelectorAll('h2');
+        accordItems = accordion.querySelectorAll('h2');
 
-    allHeaders.forEach((item, i) => {
-        item.addEventListener('click', () => {
-            if(item.classList.contains('msg-active')){
-                item.classList.remove('msg-active');
-            }else{
-                if(i > 0){
-                    allHeaders[i - 1].classList.remove('msg-active');
+    accordion.addEventListener('click', (event) => {
+        let target = event.target;
+
+        if(target.tagName.toLowerCase() === 'h2'){
+            accordItems.forEach(item => {
+                if(item !== target){
+                    item.classList.remove('msg-active');
                 }
+            });
 
-                item.classList.add('msg-active');
-            }
-        });
+            target.classList.toggle('msg-active');
+        }
     });
-
 };
 
 export default accordionSwitch;
