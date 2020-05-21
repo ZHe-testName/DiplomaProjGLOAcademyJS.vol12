5b6a2c327d04d47ac87753c339287f2696b572fa
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-21 20:28:34 +0300

    Optimized functional in formula block.

--- DiplomaProject/src/modules/formula.js
+++ DiplomaProject/src/modules/formula.js
@@ -1,56 +1,47 @@
 const formulaBlockActions = () => {
     const formula = document.querySelector('#formula'),
         dtFormulaField = formula.querySelector('.tablet-hide'),
-        formulaItems = dtFormulaField.querySelectorAll('.formula-item__icon-inner-text'),
+        formulaItems = dtFormulaField.querySelectorAll('.formula-item__icon'),
         formulaItemCards = dtFormulaField.querySelectorAll('.formula-item-popup');
 
-    formulaItems.forEach((item) => {
+    formulaItems.forEach((item, index) => {
         item.addEventListener('mouseenter', (event) => {
-            let target = event.target;
+                let target = event.target;
 
-            formulaItemCards.forEach(card => {
-                if(card.className.substr(-2) === target.textContent){
-                    if(Math.floor(card.getBoundingClientRect().top) > 0){
-                        card.style.visibility = 'visible';
-                        card.style.opacity = '1';
-                    }else{
-                        for(let i = 0; i < dtFormulaField.children.length; i++){
-                            let elems = dtFormulaField.children[i].querySelectorAll('span');
-
-                            elems.forEach(elem => {
-                                if(elem === target){
-                                    dtFormulaField.children[i].style.zIndex = '99';
-                                }
-                            })
+                if(Math.floor(formulaItemCards[index].getBoundingClientRect().top) > 0){
+                    formulaItemCards[index].style.visibility = 'visible';
+                    formulaItemCards[index].style.opacity = '1';
+                }else{
+                    for(let i = 0; i < dtFormulaField.children.length; i++){
+                        if(dtFormulaField.children[i] === target.parentNode.parentNode){
+                            dtFormulaField.children[i].style.zIndex = '99';
                         }
-                        card.firstChild.classList.add('rotate-formula-card-par');
-                        card.classList.add('rotate-formula-card');;
-                        card.style.visibility = 'visible';
-                        card.style.opacity = '1';
                     }
-                    
-                }
-            });
 
-        });
+                    formulaItemCards[index].firstChild.classList.add('rotate-formula-card-par');
+                    formulaItemCards[index].classList.add('rotate-formula-card');;
+                    formulaItemCards[index].style.visibility = 'visible';
+                    formulaItemCards[index].style.opacity = '1';
+                }
+                    
+            }
+
+        );
         
         item.addEventListener('mouseleave', (event) => {
             let target = event.target;
     
-            formulaItemCards.forEach(card => {
-                if(card.className.search(`${target.textContent}`) >= 0){
-                    card.style.visibility = 'hidden';
-                    card.style.opacity = '0';
-                    card.classList.remove('rotate-formula-card');
-                    card.firstChild.classList.remove('rotate-formula-card-par');
+            formulaItemCards[index].style.visibility = 'hidden';
+            formulaItemCards[index].style.opacity = '0';
+            formulaItemCards[index].classList.remove('rotate-formula-card');
+            formulaItemCards[index].firstChild.classList.remove('rotate-formula-card-par');
 
-                    for(let i = 0; i < dtFormulaField.children.length; i++){
-                        if(dtFormulaField.children[i].hasAttribute('style')){
-                            dtFormulaField.children[i].removeAttribute('style');
-                        };
-                    }
-                }
-            });
+            for(let i = 0; i < dtFormulaField.children.length; i++){
+                if(dtFormulaField.children[i].hasAttribute('style')){
+                    dtFormulaField.children[i].removeAttribute('style');
+                };
+            }
+
         });
     })
 };
