f2c9ba09b9024a93e0114fc46b9c9bec419c3003
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-05-22 14:55:04 +0300

    Include portfolio slider functional.

--- DiplomaProject/src/modules/documentsPopUpSlider.js
+++ DiplomaProject/src/modules/documentsPopUpSlider.js
@@ -11,6 +11,23 @@ const popUpDocsWindow = () => {
     
     docBlockPopUpSlider.init();
     docBlockPopUpSlider.zheStyles('zhe-docs-slider-style', 'zhe-docs-wrap-style', 'zhe-docs-slider-slide-style');
+    docBlockPopUpSlider.incertStyle('repair-custom-documents-popoup-style__zhe', `
+    .zhe-docs-slider-style{
+        margin: auto;
+    }
+    .zhe-docs-wrap-style{
+        display: flex;
+        transition: transform .5s;
+        will-change: transform;
+    }
+
+    .zhe-docs-slider-slide-style{
+        flex: 0 0 100%;
+        margin: auto;
+        transition: transform .5s;
+        will-change: transform;
+    }
+`);
     docBlockPopUpSlider.countShow();
 };
 
--- DiplomaProject/src/modules/portfolioSlider.js
+++ DiplomaProject/src/modules/portfolioSlider.js
@@ -3,7 +3,9 @@ import Slider from '../plugins/sliderClass';
 const portfolioSlider = () => {
     const portfolio = document.getElementById('portfolio'),
         portfolioSlider = portfolio.querySelector('.portfolio-slider-wrap'),
-        portfolioTabs = portfolio.querySelectorAll('.portfolio-slider__slide');
+        portfolioTabs = portfolio.querySelectorAll('.portfolio-slider__slide'),
+        portfolioLeftArrow = portfolio.querySelector('#portfolio-arrow_left'),
+        portfolioRightArrow = portfolio.querySelector('#portfolio-arrow_right');
 
     const mainSlider = new Slider({
         main: '.portfolio-slider-wrap',
@@ -26,12 +28,36 @@ const portfolioSlider = () => {
     }
 
     .zhe-portfolio-slider-slide-style{
-        flex: 0 0 30%;
+        flex: 0 0 33%;
         margin: auto;
         transition: transform .5s;
         will-change: transform;
     }
 `);
+
+portfolioSlider.addEventListener('click', (event) => {
+    let target = event.target;
+    let clientX;
+
+    if(target.classList.contains('slider-arrow_right-portfolio')){
+        portfolioLeftArrow.style.display = 'flex';
+
+        clientX = portfolioTabs[portfolioTabs.length - 1].getBoundingClientRect().right - portfolioSlider.getBoundingClientRect().right;
+
+        if(clientX < 380){
+            portfolioRightArrow.style.display = 'none';
+        }
+        
+    }else if(target.classList.contains('slider-arrow_left-portfolio')){
+        let zeroPoint = portfolioTabs[0].style.transform.substr(-4);
+
+        portfolioRightArrow.removeAttribute('style');
+
+        if(zeroPoint === '(0%)'){
+            portfolioLeftArrow.removeAttribute('style');
+        };
+    }
+});
 
 };
 
